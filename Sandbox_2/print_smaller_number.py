b4c04741475e5c3d5673caa6a8d99815190841b7
Coro-sensei <acoro1719@gmail.com>
2025-03-12 00:21:23 +0800

    input number

--- Sandbox_2/print_smaller_number.py
+++ Sandbox_2/print_smaller_number.py
@@ -1,3 +1,5 @@
 # Create a program that ask user to input 2 numbers. Print the smaller number.
 # Input two numbers
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
 # Print the smaller number
