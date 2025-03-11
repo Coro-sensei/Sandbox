d4226474a34d801885f007e86c3211f040b6d91d
Coro-sensei <acoro1719@gmail.com>
2025-03-12 00:15:31 +0800

    Input number

--- Sandbox_1/print_bigger_number.py
+++ Sandbox_1/print_bigger_number.py
@@ -1,3 +1,5 @@
 # Create a program that ask user to input 2 numbers. Print the bigger number.
 # Input two numbers
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
 # Print bigger number
