a32ccbdf916d4df7586fbe18eb1d6f7de4f139aa
Coro-sensei <acoro1719@gmail.com>
2025-03-12 19:31:14 +0800

    sum of all numbers added, and print all sum added

--- Sandbox_1/print_sum_of_all_numbers.py
+++ Sandbox_1/print_sum_of_all_numbers.py
@@ -1,8 +1,14 @@
 # Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
+#Initialize count
+
+sum_counter = 0
+
 # Input ten numbers 
 
 for i in range (10):
-    
+    sum_counter += float(input("Enter the ten numbers: "))
 
-# Print the sum of all ten numbers
+
+#  Print the sum of the ten numbers
+print (sum_counter)
