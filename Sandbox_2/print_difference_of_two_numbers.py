8b260b89740fca27390fd34289bbe33811ff77da
Coro-sensei <acoro1719@gmail.com>
2025-03-13 01:15:20 +0800

    test update

--- Sandbox_2/print_difference_of_two_numbers.py
+++ Sandbox_2/print_difference_of_two_numbers.py
@@ -6,4 +6,5 @@ num1 = float(input("Enter First Number: "))
 num2 = float(input("Enter Second Number: "))
 
 # Print the difference of two number
+
 print("The difference of the two numbers:", (num1 - num2))
