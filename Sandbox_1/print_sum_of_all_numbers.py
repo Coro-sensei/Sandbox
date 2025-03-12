ea9a670ada3677e3a0cdc1532169de63657217a8
Coro-sensei <acoro1719@gmail.com>
2025-03-12 19:27:04 +0800

    finished sum, forgot to commit input number

--- Sandbox_1/print_sum_of_all_numbers.py
+++ Sandbox_1/print_sum_of_all_numbers.py
@@ -1,3 +1,8 @@
 # Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
 # Input ten numbers 
+
+for i in range (10):
+    
+
 # Print the sum of all ten numbers
--- Sandbox_1/print_sum_of_two_numbers.py
+++ Sandbox_1/print_sum_of_two_numbers.py
@@ -1,3 +1,9 @@
 # Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
-# Input two numbers 
+
+# Input two numbers
+
+num1 = float(input("Enter the First Number: ")) 
+num2 = float(input("Enter the Second Number: ")) 
+
 # Print the sum of the two numbers 
+print("The sum of the two numbers: ", (num1+num2))
