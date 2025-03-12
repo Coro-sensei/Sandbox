78839e315dfb7e5f4724333af4013e2b7a429e24
Coro-sensei <acoro1719@gmail.com>
2025-03-13 00:56:48 +0800

    fixed the print odd numbers

--- Sandbox_2/print_0_100_odd_numbers_while_loop.py
+++ Sandbox_2/print_0_100_odd_numbers_while_loop.py
@@ -1,10 +1,15 @@
 # Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
 # Initiate the variable to zero
-num = 0
+num = 1
 
 # Print all odd numbers with while loop
 
+while num <=100:
+
+    print(num)
+    
+    num += 2
 
 
 
