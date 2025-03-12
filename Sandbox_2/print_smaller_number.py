20a963c180250e828a2895270303eadf6e8463d3
Coro-sensei <acoro1719@gmail.com>
2025-03-13 00:59:45 +0800

    fixed spacing for readability

--- Sandbox_2/print_smaller_number.py
+++ Sandbox_2/print_smaller_number.py
@@ -1,6 +1,10 @@
 # Create a program that ask user to input 2 numbers. Print the smaller number.
+
 # Input two numbers
+
 num1 = float(input("Enter First Number: "))
 num2 = float(input("Enter Second Number: "))
+
 # Print the smaller number
+
 print (min (num1, num2))
