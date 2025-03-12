af06af76253a39c7cafd0f79780fb0bbb036a76e
Coro-sensei <acoro1719@gmail.com>
2025-03-12 13:31:58 +0800

    input numbers

--- Sandbox_1/print_equal_if_numbers_are_equal.py
+++ Sandbox_1/print_equal_if_numbers_are_equal.py
@@ -1,3 +1,5 @@
 # Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 # Input two numbers 
+num1 = float(input("Enter the First Number: "))
+num2 = float(input("Enter the Second Number: "))
 # Print if the numbers are equal
