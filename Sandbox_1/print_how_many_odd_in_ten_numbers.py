89246c98e6067d3925c11eb43061402c1b98cd8e
Coro-sensei <acoro1719@gmail.com>
2025-03-12 18:34:03 +0800

    input number and check for odd

--- Sandbox_1/print_how_many_odd_in_ten_numbers.py
+++ Sandbox_1/print_how_many_odd_in_ten_numbers.py
@@ -1,3 +1,11 @@
 # Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+# Initialize count to zero
+odd_counter = 0
 # Input ten numbers
+for odd in range(10):
+    num = float(input("Enter the Number: {i+1}")) # The curly bracket is for the set 
+    if num % 2 != 0: # Check the remainder if the number is odd
+        odd_counter += 1
+
 # Print the amount of odd numbers
