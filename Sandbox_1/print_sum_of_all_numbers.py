26281d1d3489a146a59b6f363c321e3590e4dd78
Coro-sensei <acoro1719@gmail.com>
2025-03-12 14:33:33 +0800

    pseudocode

--- Sandbox_1/print_sum_of_all_numbers.py
+++ Sandbox_1/print_sum_of_all_numbers.py
@@ -1,3 +1,3 @@
 # Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 # Input ten numbers 
-# Print the sum of all 10 numbers
+# Print the sum of all ten numbers
---
+++ Sandbox_3/display_unique_if_number_is unique.py
@@ -0,0 +1,3 @@
+# Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+# Input a number continue asking till invalid
+# Print number then add if the number has a duplicate or is unique entry 
