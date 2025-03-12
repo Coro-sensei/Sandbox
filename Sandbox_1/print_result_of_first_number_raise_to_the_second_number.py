bb7740796ef459de44c4180622b4c4ea75f4f4c6
Coro-sensei <acoro1719@gmail.com>
2025-03-12 19:09:07 +0800

    input number

--- Sandbox_1/print_result_of_first_number_raise_to_the_second_number.py
+++ Sandbox_1/print_result_of_first_number_raise_to_the_second_number.py
@@ -1,3 +1,5 @@
 # Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 # Input the two numbers
+num1 = float(input("Enter the First Number: "))
+num2 = float(input("Enter the Second Number: "))
 # Print the result, first number raised to the second number
