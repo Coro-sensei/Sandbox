1cbc16da02d77b8a6d6ed27a8d0661677c23aaa2
Coro-sensei <acoro1719@gmail.com>
2025-03-13 01:26:13 +0800

    added the checker for even, print the even

--- Sandbox_2/print_how_many_even_numbers.py
+++ Sandbox_2/print_how_many_even_numbers.py
@@ -1,3 +1,15 @@
 # Create a program that ask user to input 10 numbers. Print how many are even numbers.
-# Input ten numbers 
+
+# Initialize to zero
+even_counter = 0
+
+# Input ten numbers
+
+for i in range(10):
+
+    num = float(input("Enter ten numbers: "))
+    if num % 2 == 0: # Check for even numbers 
+        even_counter += 1 # Keep track of count
+
 # Print how many even numbers
+print(even_counter)
