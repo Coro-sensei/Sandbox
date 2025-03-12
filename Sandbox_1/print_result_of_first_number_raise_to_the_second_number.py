6c0af05a62557c9c4b7c15611975681e8b420a83
Coro-sensei <acoro1719@gmail.com>
2025-03-12 19:15:06 +0800

    print reuslt

--- Sandbox_1/print_result_of_first_number_raise_to_the_second_number.py
+++ Sandbox_1/print_result_of_first_number_raise_to_the_second_number.py
@@ -3,3 +3,4 @@
 num1 = float(input("Enter the First Number: "))
 num2 = float(input("Enter the Second Number: "))
 # Print the result, first number raised to the second number
+print("The result of the two numbers, when first number is  raised to the second:", (num1**num2) )
