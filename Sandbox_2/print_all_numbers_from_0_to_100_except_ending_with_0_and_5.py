3c808be05ec9fe4a829436e6dd80f32a13b2f442
Coro-sensei <acoro1719@gmail.com>
2025-03-13 01:04:44 +0800

    added for range, checks for 10 and 5 and print

--- Sandbox_2/print_all_numbers_from_0_to_100_except_ending_with_0_and_5.py
+++ Sandbox_2/print_all_numbers_from_0_to_100_except_ending_with_0_and_5.py
@@ -1,2 +1,13 @@
 # Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+
+# Initiate variable 
+
+num = 0
+
 # Print all numbers except numbers ending with zero and five
+
+for num in range(101):
+
+    if num % 10 != 0 and num % 10 != 5: # Checks for fives and zeroes
+        
+        print(num)
