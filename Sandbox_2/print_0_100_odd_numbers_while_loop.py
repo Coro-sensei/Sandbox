a45e7cbeee4c5ccb6a021be8a035a587bb40551c
Coro-sensei <acoro1719@gmail.com>
2025-03-13 00:58:21 +0800

    fixed notes on first line

--- Sandbox_2/print_0_100_odd_numbers_while_loop.py
+++ Sandbox_2/print_0_100_odd_numbers_while_loop.py
@@ -1,6 +1,6 @@
 # Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
-# Initiate the variable to zero
+# Initiate the variable to one
 num = 1
 
 # Print all odd numbers with while loop
