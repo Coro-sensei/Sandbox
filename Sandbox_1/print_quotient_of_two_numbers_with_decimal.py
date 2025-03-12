82b52104ab28f14123d7e4644703613a7aab3710
Coro-sensei <acoro1719@gmail.com>
2025-03-12 18:57:40 +0800

    input numbers

--- Sandbox_1/print_quotient_of_two_numbers_with_decimal.py
+++ Sandbox_1/print_quotient_of_two_numbers_with_decimal.py
@@ -1,3 +1,5 @@
 # Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
 # Input two numbers
+num1 = float(input("Enter the First Number: "))
+num2 = float(input("Enter the Second Number: "))
 # Print the quotient of the number with decimal point.
