c9fe5482d04fc626aa00d373c4864930b2a14ddf
Coro-sensei <acoro1719@gmail.com>
2025-03-12 18:49:29 +0800

    input number

--- Sandbox_1/print_product_of_the_two_numbers.py
+++ Sandbox_1/print_product_of_the_two_numbers.py
@@ -1,3 +1,6 @@
 # Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
 # Input two numbers 
+num1 = float(input("Enter the First Number: "))
+num2 = float(input("Enter the Second Number: "))
 # Print the product of the two numbers 
