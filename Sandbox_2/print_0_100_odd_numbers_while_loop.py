18ab1cb82b24129bf5884a60243ffbce03843645
Coro-sensei <acoro1719@gmail.com>
2025-03-12 21:25:18 +0800

    initiate variable to zero

--- Sandbox_2/print_0_100_odd_numbers_while_loop.py
+++ Sandbox_2/print_0_100_odd_numbers_while_loop.py
@@ -1,2 +1,11 @@
 # Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
+# Initiate the variable to zero
+num = 0
+
 # Print all odd numbers with while loop
+
+
+
+
+
