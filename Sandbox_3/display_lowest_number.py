b2ba9c14448d7709f4c20a6daf5572411d3ba09d
Coro-sensei <acoro1719@gmail.com>
2025-03-14 07:50:28 +0800

    added while true, input number, and append to variable

--- Sandbox_3/display_lowest_number.py
+++ Sandbox_3/display_lowest_number.py
@@ -1,3 +1,13 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+# Initialize th variable to zero
+
+numbers = 0
+
 # Input a number, continue until invalid
+
+while True:
+    num = float(input("Input a number (to stop input non-numeric input): "))
+    numbers.append(num) # Adds number to the variable numbers
+
 # Print lowest number
