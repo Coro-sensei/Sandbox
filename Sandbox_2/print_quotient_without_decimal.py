83291c9a88d8bdb03bf2695d3253821b7bc4d2b4
Coro-sensei <acoro1719@gmail.com>
2025-03-13 17:22:48 +0800

    fixed difference to quotient in note

--- Sandbox_2/print_quotient_without_decimal.py
+++ Sandbox_2/print_quotient_without_decimal.py
@@ -1,3 +1,7 @@
-# Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers.
+
 # Input two numbers without decimal
+
+
+
 # Print the quotient 
