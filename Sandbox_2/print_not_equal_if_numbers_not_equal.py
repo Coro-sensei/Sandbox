9812f76d193c9565f05c7328a010929a9533efe3
Coro-sensei <acoro1719@gmail.com>
2025-03-13 17:17:54 +0800

    input number

--- Sandbox_2/print_not_equal_if_numbers_not_equal.py
+++ Sandbox_2/print_not_equal_if_numbers_not_equal.py
@@ -1,3 +1,8 @@
 # Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
 # Input two numbers 
+
+num1 = float(input("Enter the First Number: "))
+num2 = float(input("Enter the Second Number: "))
+
 # Print not even if not the same
