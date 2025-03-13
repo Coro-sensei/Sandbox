6f2d4bf7bca8e22da0ffe9dc7845600704cb7f06
Coro-sensei <acoro1719@gmail.com>
2025-03-13 19:25:21 +0800

    input numbers

--- Sandbox_2/print_the reaminder_when_the_first_number_divided_by_second_number.py
+++ Sandbox_2/print_the reaminder_when_the_first_number_divided_by_second_number.py
@@ -1,3 +1,9 @@
 # Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
 # Input two numbers
+
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
+
+
 # Print remainder of the first number and second number
