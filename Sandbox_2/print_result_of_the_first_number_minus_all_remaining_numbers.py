a98f8141f41ce0470faa364650fc4917c61b1de5
Coro-sensei <acoro1719@gmail.com>
2025-03-13 18:41:31 +0800

    added input ten num, compute result and print

--- Sandbox_2/print_result_of_the_first_number_minus_all_remaining_numbers.py
+++ Sandbox_2/print_result_of_the_first_number_minus_all_remaining_numbers.py
@@ -1,3 +1,18 @@
 # Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
+# Initiate counter
+
+num_counter = []
+
 # Input ten numbers
+
+for num in range(10):
+    num = float(input("Enter a Number: "))
+    num_counter.append(num)
+
+# Compute the result 
+
+num_minus_all = num_counter[0] - sum(num_counter[1:])
+
 # Print result of first number minus all of the remaining numbers
+print("Result of the first number minus all of the remaining number: ", num_minus_all)
