41ac93f3751f0ad03143fd8b264002ad727fa3a1
Coro-sensei <acoro1719@gmail.com>
2025-03-13 17:25:32 +0800

    fixed the note, fixed float to integer, added print of the quotient

--- Sandbox_2/print_quotient_without_decimal.py
+++ Sandbox_2/print_quotient_without_decimal.py
@@ -1,7 +1,10 @@
-# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers.
+# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers Without decimal
 
 # Input two numbers without decimal
 
-
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
 
 # Print the quotient 
+
+print("The quotient of the two numbers: ", (num1 / num2))
