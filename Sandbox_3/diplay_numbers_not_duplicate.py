8a64d5853720392fca07a0ecada2e825e5e7ef5e
Coro-sensei <acoro1719@gmail.com>
2025-03-13 22:56:39 +0800

    for loop, input number

--- Sandbox_3/diplay_numbers_not_duplicate.py
+++ Sandbox_3/diplay_numbers_not_duplicate.py
@@ -1,3 +1,17 @@
 # Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+
+# Initialize variable to zero
+
+numbers = 0
+
 # Input 10 numbers
+
+for i in range(10):
+    num = float(input("Enter a number: "))
+    numbers.append(num)
+
+# Unique numbers checker 
+
+
 # Print all numbers that don't duplicate
