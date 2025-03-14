9f3cb4ef19897c824d3f79ce51d6276bc11a22bf
Coro-sensei <acoro1719@gmail.com>
2025-03-14 08:25:08 +0800

    while true input, try except valueerror

--- Sandbox_3/display_lowest{_to_highest_sort_function.py
+++ Sandbox_3/display_lowest{_to_highest_sort_function.py
@@ -1,3 +1,16 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+
+# Initialize variable to list
+
+numbers = []
+
 # Input number, continue until invalid 
+while True:
+    try:
+        num = float(input("Input a number (to stop, input non-numeric value): "))
+        numbers.append(num)  # Add number to the list
+    except ValueError:
+        print("Stopping input...")
+        break  # Stop when the user enters an invalid input
+
 # Print lowest to highest with sort() function
