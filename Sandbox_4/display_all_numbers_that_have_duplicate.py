c6bd51b3b39d551c8531f4d66f165589c58777cf
Coro-sensei <acoro1719@gmail.com>
2025-03-14 09:50:09 +0800

    added, for in range input, adds dups to list, print dups

--- Sandbox_4/display_all_numbers_that_have_duplicate.py
+++ Sandbox_4/display_all_numbers_that_have_duplicate.py
@@ -1,4 +1,21 @@
 # Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
 # Initialize variable to list
+
+numbers = []
+duplicates = set() # Set for duplicates
+
 # Input ten numbers, check for duplicates
+
+for i in range(10):
+    num = float(input("Enter a number: "))
+    if num in numbers:
+        duplicates.add(num) # Add to duplicate numbers list
+    numbers.append(num) # Store all numbers
+
 # Print all numbers that have a duplicate
+
+if duplicates:
+    print("Numbers with duplicates: ", list(duplicates))
+else:
+    print("No duplicates found. ")
