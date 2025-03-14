2ea6264925008e494d1c80c89593bf96f78e5c6f
Coro-sensei <acoro1719@gmail.com>
2025-03-14 08:43:58 +0800

    added input, added detection of unique and dups, break at invalid

--- Sandbox_3/display_unique_if_number_is unique.py
+++ Sandbox_3/display_unique_if_number_is unique.py
@@ -1,6 +1,20 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 
-# Input a number continue asking till invalid
+# Initialize variable to list
 
+numbers = []
 
-# Print number then add if the number has a duplicate or is unique entry 
+# Input a number continue asking until invalid
+
+while True:
+    try:
+        num = float(input("Input a number (to stop, input non-numeric input):"))
+        if num in numbers:
+            print("Duplicate")
+        else:
+            print("Unique")
+            numbers.append(num) # Adds unique to numbers
+    except ValueError:
+        print 
+        break # Stops at invalid input
+
