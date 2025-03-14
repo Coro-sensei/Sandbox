6ca40e8a336bfcf855708a6c1b9ad475366ccf58
Coro-sensei <acoro1719@gmail.com>
2025-03-14 08:30:37 +0800

    init var to list, added input 10 numbers

--- Sandbox_3/display_numbers_with_duplicate_display_only_first_entry.py
+++ Sandbox_3/display_numbers_with_duplicate_display_only_first_entry.py
@@ -1,6 +1,13 @@
 # Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry
 
+# Initialize variable to list
+
+numbers = []
+
 # Input ten numbers
 
+for i in range(10):
+    num = float(input("Enter a number: "))
+    numbers.append(num)
 
 # Print ten number, for duplicates display the first entry of that number
