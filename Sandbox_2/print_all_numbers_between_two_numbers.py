353dff8c42fd6b8bef6f7ffe8e5ff90e71a48eea
Coro-sensei <acoro1719@gmail.com>
2025-03-26 19:16:42 +0800

    modified:   add input, find min max, print all between the numbers

--- Sandbox_2/print_all_numbers_between_two_numbers.py
+++ Sandbox_2/print_all_numbers_between_two_numbers.py
@@ -1,5 +1,16 @@
 # Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
 # Input two numbers 
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Make it so the smaller number is first
+
+start = min(num1, num2)
+end = max(num1, num2)
+
 # Print the result 
+print("Numbers between {start} and {end}: ")
+for i in range(start+1, end):
+    print(i, end = " ")
