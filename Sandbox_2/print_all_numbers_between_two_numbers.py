13c59846611bdddb813db5e8f5fd8047e0cf918d
Coro-sensei <acoro1719@gmail.com>
2025-03-26 19:11:50 +0800

    new file:   pseudocode

---
+++ Sandbox_2/print_all_numbers_between_two_numbers.py
@@ -0,0 +1,5 @@
+# Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+# Input two numbers 
+# Make it so the smaller number is first
+# Print the result 
